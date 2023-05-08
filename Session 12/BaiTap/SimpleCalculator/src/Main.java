a88c06474acd2be5c8b0408d62e13143be61fc40
Mac <trongnhan34vn@gmail.com>
2023-05-08 08:04:57 +0700

    update

---
+++ Session 12/BaiTap/SimpleCalculator/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("-------------- Simple Calculator ---------------");
+        System.out.println("1. Plus");
+        System.out.println("2. Subtract");
+        System.out.println("3. Multiple");
+        System.out.println("4. Divide");
+        System.out.println("-------------- Simple Calculator ---------------");
+        System.out.println("Enter first number: ");
+        int firstNum = scanner.nextInt();
+        System.out.println("Enter your choice: ");
+        int choice = scanner.nextInt();
+        switch (choice) {
+            case 1:
+                
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+        }
+    }
+}
