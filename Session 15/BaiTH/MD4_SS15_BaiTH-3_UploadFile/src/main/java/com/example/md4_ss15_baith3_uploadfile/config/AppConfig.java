1f1dbff2efd0a499c4619018ae168ca098fc7b0e
Nhan <nhan@iMac-Pro.local>
2023-05-10 00:28:30 +0700

    update

--- Session 15/BaiTH/MD4_SS15_BaiTH-3_UploadFile/src/main/java/com/example/md4_ss15_baith3_uploadfile/config/AppConfig.java
+++ Session 15/BaiTH/MD4_SS15_BaiTH-3_UploadFile/src/main/java/com/example/md4_ss15_baith3_uploadfile/config/AppConfig.java
@@ -1,25 +1,34 @@
 package com.example.md4_ss15_baith3_uploadfile.config;
 
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
+import java.io.IOException;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.example.md4_ss15_baith3_uploadfile.controller")
-public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
-    @Autowired
+@PropertySource("classpath:upload_file.properties")
+public class AppConfig implements WebMvcConfigurer,  ApplicationContextAware {
+    @Value("${file-upload}")
+    private String fileUpload;
+
     private ApplicationContext applicationContext;
+
     @Override
     public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
         this.applicationContext = applicationContext;
@@ -52,4 +61,19 @@ public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
         return viewResolver;
     }
 
+    //Cấu hình upload file
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:" + fileUpload);
+
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver getResolver() throws IOException {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setMaxUploadSizePerFile(52428800);
+        return resolver;
+    }
 }
+
